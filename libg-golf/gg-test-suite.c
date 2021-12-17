bb120d811d4fda53a7e5abd66259fbfd2e76bb71
David Pirotte <david@altosw.be>
2021-12-16 23:41:46 -0300

    Reorganizing libg-golf
      Splitting the functionality that was in libg-golf.c in gg-*.c
      files. So far so good, libg-golf just had a few lines of C code, but
      adding G-Golf support for GICallbackInfo arguments will require a bit
      more C code - time to better organize 'things' on the C side.
    
    * libg-golf/libg-golf.c
    * libg-golf/libg-golf.h: Removed. The functionality has been moved in
      their respective new file below.
    
    * libg-golf/gg-glib.c:
    * libg-golf/gg-glib.h:
    * libg-golf/gg-gobject.c:
    * libg-golf/gg-gobject.h:
    * libg-golf/gg-utils.c:
    * libg-golf/gg-utils.h:
    * libg-golf/gg-test-suite.c:
    * libg-golf/gg-test-suite.h: New files, each holding receiving
    
    * libg-golf/g-golf.c: Currently empty, just including the newly created
      gg-*.h files. I could get rid of it, but i'll keep it (for now), in
      case I use libguile and need to init ... we'll see.
    
    * libg-golf/Makefile.am: Adapted to cope with the new organization.
    
    * .gitignore: Do not track libg-golf/libg_golf_la-*.lo, not just
      libg-golf/libg_golf_la-libg-golf.lo

---
+++ libg-golf/gg-test-suite.c
@@ -0,0 +1,71 @@
+/*  -*- mode: C; coding: utf-8 -*-
+
+####
+#### Copyright (C) 2021
+#### Free Software Foundation, Inc.
+
+#### This file is part of GNU G-Golf.
+
+#### GNU G-Golf is free software; you can redistribute it and/or
+#### modify it under the terms of the GNU General Public License as
+#### published by the Free Software Foundation; either version 3 of
+#### the License, or (at your option) any later version.
+
+#### GNU G-Golf is distributed in the hope that it will be useful, but
+#### WITHOUT ANY WARRANTY; without even the implied warranty of
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#### General Public License for more details.
+
+#### You should have received a copy of the GNU General Public License
+#### along with GNU G-Golf.  If not, see
+#### <https://www.gnu.org/licenses/gpl.html>.
+####
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+
+#include <glib.h>
+#include <glib-object.h>
+
+
+/*
+ * Test suite
+ *
+*/
+
+char**
+test_suite_n_string_ptr ()
+{
+  char** strings = malloc(3 * sizeof(char*));
+
+  for (int i = 0 ; i < 3; ++i)
+    strings[i] = malloc(3 * sizeof(char));
+
+  strings[0] = "foo";
+  strings[1] = "bar";
+  strings[2] = "baz";
+
+  return strings;
+}
+
+char**
+test_suite_strings_ptr ()
+{
+  char** strings = malloc(4 * sizeof(char*));
+
+  for (int i = 0 ; i < 3; ++i)
+    strings[i] = malloc(3 * sizeof(char));
+
+  strings[0] = "foo";
+  strings[1] = "bar";
+  strings[2] = "baz";
+  strings[3] = NULL;
+
+  return strings;
+}
