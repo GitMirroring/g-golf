6a5f1c1e719e81fc41460cd25dbf2095def2aee9
David Pirotte <david@altosw.be>
2022-05-26 22:07:22 -0300

    Import/init <callback> (wip) - New <callback> related caches
    * g-golf/hl-api/callback.scm (gi-import-callback): Either retrieve or
      create a <callback> instance for info (GICallbackInfo).
    
      (initialize <callback>): New method - Wip. So far, as <callback>
      inherit from <callable>, it merely runs the <callable> initialization
      machinery, that is, create/fills a <callback> instance/slots,
      retreives and creates its <argument> instances - Wip.
    
      (gi-callback-inst-cache-ref, gi-callback-inst-cache-set!,
      gi-callback-inst-cache-remove!, gi-callback-inst-cache-for-each,
      gi-callback-inst-cache-show): A <callback> instance cache.
    
      (gi-callback-closure-cache-ref, gi-callback-closure-cache-set!,
      gi-callback-closure-cache-remove!, gi-callback-closure-cache-for-each,
      gi-callback-closure-cache-show): A callabck (user) closure cache.

--- g-golf/hl-api/callback.scm
+++ g-golf/hl-api/callback.scm
@@ -1,7 +1,7 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 
 ;;;;
-;;;; Copyright (C) 2019 - 2021
+;;;; Copyright (C) 2019 - 2022
 ;;;; Free Software Foundation, Inc.
 
 ;;;; This file is part of GNU G-Golf
@@ -27,6 +27,7 @@
 
 
 (define-module (g-golf hl-api callback)
+  #:use-module (ice-9 threads)
   #:use-module (ice-9 match)
   #:use-module (ice-9 receive)
   #:use-module (srfi srfi-1)
@@ -36,26 +37,71 @@
   #:use-module (g-golf glib)
   #:use-module (g-golf gobject)
   #:use-module (g-golf support libg-golf)
+  #:use-module (g-golf hl-api n-decl)
   #:use-module (g-golf hl-api gtype)
   #:use-module (g-golf hl-api gobject)
+  #:use-module (g-golf hl-api events)
+  #:use-module (g-golf hl-api argument)
+  #:use-module (g-golf hl-api ccc)
 
   #:duplicates (merge-generics
 		replace
 		warn-override-core
 		warn
 		last)
-  
+
   #:export (gi-import-callback
-            g-golf-callback-closure))
+
+            g-golf-callback-closure
+
+            ;; <callback> inst cache
+            gi-callback-inst-cache-ref
+            gi-callback-inst-cache-set!
+            gi-callback-inst-cache-remove!
+            gi-callback-inst-cache-for-each
+            gi-callback-inst-cache-show
+
+            ;; callabck (user) closure cache
+            gi-callback-closure-cache-ref
+            gi-callback-closure-cache-set!
+            gi-callback-closure-cache-remove!
+            gi-callback-closure-cache-for-each
+            gi-callback-closure-cache-show))
 
 
 #;(g-export )
 
 
 (define (gi-import-callback info)
-  (dimfi (g-base-info-get-namespace info)
-         (g-base-info-get-name info)
-         " (callback)"))
+  (let* ((namespace (g-base-info-get-namespace info))
+         (g-name (g-base-info-get-name info))
+         (name (g-name->name g-name)))
+    (dimfi 'import-callback namespace name)
+    (or (gi-callback-inst-cache-ref name)
+        (let ((callback (make <callback> #:info info
+                              #:namespace namespace
+                              #:g-name g-name
+                              #:name name)))
+          ;; Do not (g-base-info-)unref the callback info - it is
+          ;; required when invoked.
+          (gi-callback-inst-cache-set! name callback)
+          callback))))
+
+(define-method (initialize (self <callback>) initargs)
+  (let ((info (or (get-keyword #:info initargs #f)
+                  (error "Missing #:info initarg: " initargs)))
+        (namespace (get-keyword #:name initargs #f))
+        (name (get-keyword #:name initargs #f)))
+    (if name
+        (next-method)
+        (let* ((namespace (g-base-info-get-namespace info))
+               (g-name (g-base-info-get-name info))
+               (name (g-name->name g-name)))
+          (next-method self
+                       (append initargs
+                               `(#:namespace ,namespace
+                                 #:g-name ,g-name
+                                 #:name ,name)))))))
 
 
 ;;;
@@ -65,6 +111,64 @@
 (define (g-golf-callback-closure name info proc)
   (dimfi "g-golf-callback-closure")
   (dimfi "  " name " " (g-base-info-get-name info))
+  (let ((callback (gi-import-callback info)))
+    (describe callback))
+  ;; Currently returns NULL - Wip ...
   (g_golf_callback_closure (string->pointer (symbol->string name))
                            info		;; a GICallbackInfo ptr
                            (scm->pointer proc)))
+
+
+;;;
+;;; The gi-callback-inst-cache
+;;;
+
+(define %gi-callback-inst-cache-default-size 1013)
+
+(define %gi-callback-inst-cache #f)
+(define gi-callback-inst-cache-ref #f)
+(define gi-callback-inst-cache-set! #f)
+(define gi-callback-inst-cache-remove! #f)
+(define gi-callback-inst-cache-for-each #f)
+
+(let* ((gi-callback-size %gi-callback-inst-cache-default-size)
+       (gi-callback-inst-cache (make-hash-table
+                                %gi-callback-inst-cache-default-size))
+       (gi-callback-inst-mutex (make-mutex)))
+
+  (set! %gi-callback-inst-cache
+        (lambda () gi-callback-inst-cache))
+
+  (set! gi-callback-inst-cache-ref
+        (lambda (name)
+          (with-mutex gi-callback-inst-mutex
+            (hashq-ref gi-callback-inst-cache name))))
+
+  (set! gi-callback-inst-cache-set!
+        (lambda (name callback)
+          (with-mutex gi-callback-inst-mutex
+            (hashq-set! gi-callback-inst-cache name callback))))
+
+  (set! gi-callback-inst-cache-remove!
+        (lambda (name)
+          (with-mutex gi-callback-inst-mutex
+            (hashq-remove! gi-callback-inst-cache name))))
+
+  (set! gi-callback-inst-cache-for-each
+        (lambda (proc)
+          (with-mutex gi-callback-inst-mutex
+            (hash-for-each proc
+                           gi-callback-inst-cache)))))
+
+(define %gi-callback-inst-cache-show-prelude
+  "The <callback> inst cache entries are")
+
+(define* (gi-callback-inst-cache-show #:optional
+                                      (port (current-output-port)))
+  (format port "~A~%"
+          %gi-callback-inst-cache-show-prelude)
+  (letrec ((show (lambda (key value)
+                   (format port "  ~S  -  ~S~%"
+                           key
+                           value))))
+    (gi-callback-inst-cache-for-each show)))
