a57b0a8125b749bbf6b1fd45e8ff04b86af30016
David Pirotte <david@altosw.be>
2022-07-25 00:27:17 -0300

    Moving the gi-iface-* and cache procs to a new module
    * g-golf/hl-api/iface.scm (gi-iface-init-func-closure,
      gi-iface-vfunc-closure, gi-iface-init-func-cache-* and
      gi-iface-info-struct-cache): Newmodule. All functionality moved in
      this module, from (golf hl-api object).
    
    * Makefile.am:
    * g-golf.scm:
    * g-golf/hl-api.scm:
    * g-golf/hl-api/object.scm: Updated for the above.

--- g-golf/hl-api/object.scm
+++ g-golf/hl-api/object.scm
@@ -27,7 +27,6 @@
 
 
 (define-module (g-golf hl-api object)
-  #:use-module (ice-9 threads)
   #:use-module (ice-9 match)
   #:use-module (ice-9 receive)
   #:use-module (srfi srfi-1)
@@ -48,21 +47,7 @@
 		last)
   
   #:export (gi-import-object
-            gi-import-interface
-
-            ;; iface-init-func cache
-            gi-iface-init-func-cache-ref
-            gi-iface-init-func-cache-set!
-            gi-iface-init-func-cache-remove!
-            gi-iface-init-func-cache-for-each
-            gi-iface-init-func-cache-show
-
-            ;; iface-info-struct cache
-            gi-iface-info-struct-cache-ref
-            gi-iface-info-struct-cache-set!
-            gi-iface-info-struct-cache-remove!
-            gi-iface-info-struct-cache-for-each
-            gi-iface-info-struct-cache-show))
+            gi-import-interface))
 
 
 #;(g-export )
@@ -273,12 +258,7 @@
              (module-add! public-i c-name
                           (module-variable module c-name))
              (when with-methods?
-               (gi-import-interface-methods info #:force? force?)
-               (let ((iface-info-struct-name
-                      (symbol-append (g-name->name g-name) '-info-struct)))
-                 (gi-iface-info-struct-cache-set! iface-info-struct-name
-                                                  (g-iface-info-struct-new
-                                                   (gi-iface-init-func-closure info)))))
+               (gi-import-interface-methods info #:force? force?))
              ;; We do not import signals, they are imported on
              ;; demand. Visit (g-golf hl-api signal) signal-connect and
              ;; the %gi-signal-cache related code to see how this is
@@ -304,187 +284,3 @@
         ;; (re)import those here.
         (unless (g-irepository-find-by-name namespace name)
           (%gi-import-function m-info #:force? force?))))))
-
-(define (gi-iface-init-func-closure info)
-  (let* ((module (resolve-module '(g-golf hl-api gobject)))
-         (g-name (g-registered-type-info-get-type-name info))
-         (name (g-name->name g-name))
-         (iface-struct (g-interface-info-get-iface-struct info))
-         (field-desc (and iface-struct
-                          (gi-struct-field-desc iface-struct))))
-    (if field-desc
-        (match field-desc
-          ((g-iface . vfunc-field-desc)
-           (let loop ((vfuncs vfunc-field-desc)
-                      (vfunc-closure-desc '()))
-             (match vfuncs
-               (()
-                (let ((iface-init-func-name (symbol-append name '-init-func))
-                      (iface-init-func
-                       (gi-iface-init-func-closure-1 vfunc-closure-desc)))
-                  (gi-iface-init-func-cache-set! iface-init-func-name
-                                                 iface-init-func)
-                  iface-init-func))
-               ((vfunc . rest)
-                (loop rest
-                      (cons (gi-iface-vfunc-closure name vfunc module)
-                            vfunc-closure-desc)))))))
-        (begin
-          (warning "No iface-struct:" g-name)
-          %iface-init-func))))
-
-  (define (gi-iface-init-func-closure-1 vfunc-closure-desc)
-    (procedure->pointer void
-                        (lambda (g-iface iface-data)
-                          (let loop ((vfuncs vfunc-closure-desc))
-                            (match vfuncs
-                              (() 'done)
-                              ((vfunc . rest)
-                               (match vfunc
-                                 ((vfunc-closure . iface-struct-offset)
-                                  (bv-ptr-set! (gi-pointer-inc g-iface
-                                                               iface-struct-offset)
-                                               vfunc-closure)))
-                               (loop rest))))
-                          (values))
-                        (list '* '*)))
-
-(define (gi-iface-vfunc-closure iface-name vfunc-field-desc module)
-  (let ((%g-golf-vfunc-closure
-         (@ (g-golf hl-api callback) g-golf-vfunc-closure)))
-    (match vfunc-field-desc
-      ((name type-tag iface-struct-offset flags)
-       ;; not all, but most vfunc have a corresponding method. when this
-       ;; procedure is called, they have been imported already. we retrieve it
-       ;; and use the <function> instance (which has all the callable info we
-       ;; need to make a callback), and the method itself, as this is what
-       ;; users will specialize.
-       (let* ((method-name (symbol-append iface-name '- name))
-              (f-inst (gi-cache-ref 'function method-name))
-              (method (and f-inst (module-ref module name)))
-              ;; i initially thought all vfunc have a corresponding method,
-              ;; but i was told on #introspection there are exceptions.
-              (closure (if f-inst
-                           (%g-golf-vfunc-closure method-name
-                                                  f-inst
-                                                  (lambda args (apply method args)))
-                           (begin
-                             (warning "VFunc with no corresponding method"
-                                      (cons iface-name name))
-                             %null-pointer))))
-         (when (%debug)
-           (dimfi 'vfunc-closure 'for: method-name)
-           (dimfi "  " method)
-           (dimfi "  " f-inst 'n-arg: (slot-ref f-inst 'n-arg))
-           (dimfi "  " 'closure: closure 'offset: iface-struct-offset))
-         (cons closure iface-struct-offset))))))
-
-
-;;;
-;;; The gi-iface-init-func-cache
-;;;
-
-(define %gi-iface-init-func-cache-default-size 1013)
-
-(define %gi-iface-init-func-cache #f)
-(define gi-iface-init-func-cache-ref #f)
-(define gi-iface-init-func-cache-set! #f)
-(define gi-iface-init-func-cache-remove! #f)
-(define gi-iface-init-func-cache-for-each #f)
-
-(let* ((cache-size %gi-iface-init-func-cache-default-size)
-       (gi-iface-init-func-cache (make-hash-table
-                                %gi-iface-init-func-cache-default-size))
-       (gi-iface-init-func-mutex (make-mutex)))
-
-  (set! %gi-iface-init-func-cache
-        (lambda () gi-iface-init-func-cache))
-
-  (set! gi-iface-init-func-cache-ref
-        (lambda (name)
-          (with-mutex gi-iface-init-func-mutex
-            (hashq-ref gi-iface-init-func-cache name))))
-
-  (set! gi-iface-init-func-cache-set!
-        (lambda (name callback)
-          (with-mutex gi-iface-init-func-mutex
-            (hashq-set! gi-iface-init-func-cache name callback))))
-
-  (set! gi-iface-init-func-cache-remove!
-        (lambda (name)
-          (with-mutex gi-iface-init-func-mutex
-            (hashq-remove! gi-iface-init-func-cache name))))
-
-  (set! gi-iface-init-func-cache-for-each
-        (lambda (proc)
-          (with-mutex gi-iface-init-func-mutex
-            (hash-for-each proc
-                           gi-iface-init-func-cache)))))
-
-(define %gi-iface-init-func-cache-show-prelude
-  "The iface-init-func cache entries are")
-
-(define* (gi-iface-init-func-cache-show #:optional
-                                        (port (current-output-port)))
-  (format port "~A~%"
-          %gi-iface-init-func-cache-show-prelude)
-  (letrec ((show (lambda (key value)
-                   (format port "  ~S  -  ~S~%"
-                           key
-                           value))))
-    (gi-iface-init-func-cache-for-each show)))
-
-
-;;;
-;;; The gi-iface-init-func-cache
-;;;
-
-(define %gi-iface-info-struct-cache-default-size 1013)
-
-(define %gi-iface-info-struct-cache #f)
-(define gi-iface-info-struct-cache-ref #f)
-(define gi-iface-info-struct-cache-set! #f)
-(define gi-iface-info-struct-cache-remove! #f)
-(define gi-iface-info-struct-cache-for-each #f)
-
-(let* ((cache-size %gi-iface-info-struct-cache-default-size)
-       (gi-iface-info-struct-cache (make-hash-table
-                                %gi-iface-info-struct-cache-default-size))
-       (gi-iface-info-struct-mutex (make-mutex)))
-
-  (set! %gi-iface-info-struct-cache
-        (lambda () gi-iface-info-struct-cache))
-
-  (set! gi-iface-info-struct-cache-ref
-        (lambda (name)
-          (with-mutex gi-iface-info-struct-mutex
-            (hashq-ref gi-iface-info-struct-cache name))))
-
-  (set! gi-iface-info-struct-cache-set!
-        (lambda (name callback)
-          (with-mutex gi-iface-info-struct-mutex
-            (hashq-set! gi-iface-info-struct-cache name callback))))
-
-  (set! gi-iface-info-struct-cache-remove!
-        (lambda (name)
-          (with-mutex gi-iface-info-struct-mutex
-            (hashq-remove! gi-iface-info-struct-cache name))))
-
-  (set! gi-iface-info-struct-cache-for-each
-        (lambda (proc)
-          (with-mutex gi-iface-info-struct-mutex
-            (hash-for-each proc
-                           gi-iface-info-struct-cache)))))
-
-(define %gi-iface-info-struct-cache-show-prelude
-  "The iface-info-struct cache entries are")
-
-(define* (gi-iface-info-struct-cache-show #:optional
-                                        (port (current-output-port)))
-  (format port "~A~%"
-          %gi-iface-info-struct-cache-show-prelude)
-  (letrec ((show (lambda (key value)
-                   (format port "  ~S  -  ~S~%"
-                           key
-                           value))))
-    (gi-iface-info-struct-cache-for-each show)))
