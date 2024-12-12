cdd4efe81c942a6e472e8df5fc7dccd84a35f68c
David Pirotte <david@altosw.be>
2024-12-11 22:52:55 -0300

    Fixing gi-type-tag->bv-acc - Adding unichar
    * g-golf/gi/common-types.scm (gi-type-tag->bv-acc): Adding support for
      unichar.
    k

--- g-golf/gi/common-types.scm
+++ g-golf/gi/common-types.scm
@@ -241,7 +241,8 @@ type-tag."
     ((boolean
       int32)
      (values make-s32vector s32vector-ref s32vector-set!))
-    ((uint32)
+    ((uint32
+      unichar)
      (values make-u32vector u32vector-ref u32vector-set!))
     ((int64)
      (values make-s64vector s64vector-ref s64vector-set!))
